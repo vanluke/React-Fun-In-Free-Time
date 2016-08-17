154d0198df637375840aa9ea7392f6075b7daffb
Lukasz Gonciarz <vanluke@op.pl>
2016-08-17 14:48:47 +0200

    create app component and routes

---
+++ client/src/routes/index.js
@@ -0,0 +1,11 @@
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
+import React from 'react';
+import { App } from '../shared';
+import { Login } from '../login';
+
+export const routes = (<Router history={hashHistory}>
+  <Route path="/" component={App}>
+    <IndexRedirect to="/login" />
+    <Route path="/dashbards" component={Login} />
+  </Route>
+</Router>);
---
+++ client/src/shared/components/app.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+const App = props => {
+  return <div>{props.children}</div>;
+};
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
