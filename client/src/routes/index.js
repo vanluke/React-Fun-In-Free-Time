b4574839f9aacbbe660c3724b0b27d3db00f1ca5
Lukasz Gonciarz <vanluke@op.pl>
2016-08-19 08:23:11 +0200

    add flat dashboard component

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,10 +2,12 @@ import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
 import React from 'react';
 import { App } from '../shared';
 import { Login } from '../login';
+import { Dashboard } from '../dashboard';
 
 export const routes = (<Router history={hashHistory}>
   <Route path="/" component={App}>
     <IndexRedirect to="/login" />
     <Route path="/login" component={Login} />
+    <Route path="/dashboard" component={Dashboard} />
   </Route>
 </Router>);
