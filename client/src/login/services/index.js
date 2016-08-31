78535b7b8b4db5a215922fd3e07c507fa333e3b1
Lukasz Gonciarz <vanluke@op.pl>
2016-08-31 12:48:24 +0200

    use fetch to retreive token

--- client/src/login/services/index.js
+++ client/src/login/services/index.js
@@ -1,3 +1,15 @@
-export const authenticate = ({ userName, password }) => {
-  return Promise.resolve({ userName, password });
+import config from '../login.config';
+
+export const authenticate = (credentials) => {
+  const url = config.get('auth').url;
+  return window.fetch(`${url}`, {
+    mode: 'cors',
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }),
+    body: JSON.stringify(credentials)
+  })
+  .then(response => response.json())
+  .then(token => token);
 };
