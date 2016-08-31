ca37e11c8fe0055384e493e4dd6215b7395de17c
Lukasz Gonciarz <vanluke@op.pl>
2016-08-31 11:11:43 +0200

    update routes login route need to be on top. Change authentication routes, on enter callback add condition

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,33 +1,44 @@
 import { Router, Route, IndexRedirect,
   hashHistory, browserHistory } from 'react-router';
 import React from 'react';
-import { App, NotFoundPage, Home, AuthenticatedComponent } from '../shared';
+import { App, NotFoundPage, Home, ComponentDecorator } from '../shared';
 import { Login } from '../login';
 import { Dashboard } from '../dashboard';
 import { News } from '../news';
 import { getItemFromLocalstorage } from '../middleware';
 
+const validateLocation = (location, prevLocation) => {
+  return (location && location.pathname.indexOf('login') >= 0)
+    || (prevLocation && prevLocation.pathname.indexOf('login') >= 0);
+};
+
+const validateLoginLocation = (location) => {
+  return (location && location.pathname.indexOf('login') <= 0);
+};
+
 const checkLoginRoute = function(prev, next) {
   const isAuth = getItemFromLocalstorage('user');
   const { location } = next;
   const { location: prevLocation } = prev;
-  if (isAuth.password && (
-    (location && location.pathname.indexOf('login') >= 0)
-    || (prevLocation && prevLocation.pathname.indexOf('login') >= 0))) {
-    browserHistory.push('#/dashboard');
+  if (Object.keys(isAuth).length === 0
+    && validateLoginLocation(!!location ? location : prevLocation)) {
+    browserHistory.push('#/login');
+  } else if (Object.keys(isAuth).length > 0
+    && validateLocation(location, prevLocation)) {
+    browserHistory.push('#/home');
   }
 };
 
 export const routes = (<Router history={hashHistory}>
+  <Route path="login" component={Login} />
   <Route
-    path="/" component={App}
+    path="/" component={ComponentDecorator(App)}
     onChange={checkLoginRoute}
     onEnter={checkLoginRoute}>
-    <IndexRedirect to="/login" />
-    <Route path="/login" component={Login} />
+    <IndexRedirect to="/home" />
     <Route path="/news" component={News} />
-    <Route path="/home" component={AuthenticatedComponent(Home)} />
-    <Route path="/dashboard" component={AuthenticatedComponent(Dashboard)} />
+    <Route path="/home" component={Home} />
+    <Route path="/dashboard" component={Dashboard} />
     <Route path="*" component={NotFoundPage} />
   </Route>
 </Router>);
