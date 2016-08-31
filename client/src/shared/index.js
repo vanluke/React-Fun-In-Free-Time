88e074d51eaca58cb247aef8c69ebae0502690f8
Lukasz Gonciarz <vanluke@op.pl>
2016-08-31 11:17:22 +0200

    update exports

--- client/src/shared/index.js
+++ client/src/shared/index.js
@@ -1,6 +1,6 @@
 export { App } from './components';
 export { default as NotFoundPage } from './components/not-found';
 export { default as Home } from './components/home';
-export { default as AuthenticatedComponent }
+export { default as ComponentDecorator }
   from './components/require-authentication';
 export { ApplicationModal } from './components';
