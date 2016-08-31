28a2ad933779b096f58c4dc8b2a01c1a34e2578a
Lukasz Gonciarz <vanluke@op.pl>
2016-08-31 11:01:58 +0200

    update exports

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -3,3 +3,13 @@ export const httpGet = function(url) {
     .then(response => response.json())
     .then(json => json);
 };
+
+export {
+  createJWT,
+  readJWT
+} from './jwt';
+
+export { MongoClient, ObjectID, composeObject } from './mongodb';
+export { connect } from './mongodb-connect';
+export { default as _protected } from './protected';
+export { default as _unprotected } from './unprotected';
